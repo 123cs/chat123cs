f55c95cbe4b72d4321a45d166a660ed94fac0b58
Thuat nv <thuatnv@Thuats-MacBook-Pro.local>
2018-01-25 10:19:55 +0700

    Udpate js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+'use strict';
 
-// app.get('/', function(req, res){
-//     res.send('<h1>Hello world</h1>');
-// });
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+    .use((req, res) => res.sendFile(INDEX) )
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+    socket.on('chat message', (msg)=> (io.emit('chat message', msg)));
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-    });
-});
-
-
-http.listen(3000, function(){
-    console.log('listening on *:3000');
-});
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
