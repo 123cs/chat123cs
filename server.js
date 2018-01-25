9ef2708cc6753e587471c3ff384052487fe9a0eb
Thuat nv <thuatnv@Thuats-MacBook-Pro.local>
2018-01-25 10:50:57 +0700

    Update js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-    .use((req, res) => res.json({"error_code":0,"error_message":"Successful.","data":{}}) )
+    .use((req, res) => res.sendFile(INDEX) )
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
